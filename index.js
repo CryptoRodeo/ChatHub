debe6d2db294b0d6269b616e66e94edb8ad85c03
Bryan Ramos <b.r.tech95@gmail.com>
2020-03-17 17:34:31 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+let app = express();
+var http = require('http').createServer(app);
+var socket = require('socket.io')(http);
+app.use(express.static('node_modules'))
+app.use(express.static('public'));
+
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+  socket.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+      socket.emit('chat message', msg);
+    });
+  });
+
+http.listen(4000, function(){
+  console.log('listening on *:3000');
+});
---
+++ public/scripts/socket_frontend.js
@@ -0,0 +1,14 @@
+(()=> {
+    let socket = io();
+    document.querySelector("form").addEventListener('submit', (e) => {
+        e.preventDefault();
+        let submit_button = document.querySelector("#m");
+        socket.emit('chat message', submit_button.value);
+        submit_button.value = '';
+        return false;
+    });
+        socket.on('chat message', (msg) => {
+            let message = `<li>${msg}</li>`; 
+            document.querySelector("#messages").insertAdjacentHTML('beforeend', message)
+        });
+})();
