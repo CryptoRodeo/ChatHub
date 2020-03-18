0839f8c0fb8011a6d4655dc313d86212c21bdc63
Bryan Ramos <b.r.tech95@gmail.com>
2020-03-18 11:48:22 -0400

    app crudley holds usernames

--- public/scripts/socket_frontend.js
+++ public/scripts/socket_frontend.js
@@ -1,6 +1,10 @@
 (()=> {
+    const queryString = window.location.href;
+    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    const username = urlParams.get('username');
     let socket = io();
-    document.querySelector("form").addEventListener('submit', (e) => {
+    document.querySelector("#chatform").addEventListener('submit', (e) => {
         e.preventDefault();
         let submit_button = document.querySelector("#m");
         socket.emit('chat message', submit_button.value);
@@ -8,7 +12,7 @@
         return false;
     });
         socket.on('chat message', (msg) => {
-            let message = `<li>${msg}</li>`; 
+            let message = `<li>${username}:     ${msg}</li>`; 
             document.querySelector("#messages").insertAdjacentHTML('beforeend', message)
         });
         document.addEventListener('visibilitychange', (e) => {
--- server/index.js
+++ server/index.js
@@ -9,14 +9,31 @@ let socket = require('socket.io')(http);
 app.use(partials());
 app.use(express.static(__dirname + '/../node_modules'))
 app.use(express.static(__dirname + '/../public'));
+app.use(express.json());
+app.use(express.urlencoded());
 
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/../views');
 
+let pass_user_date = (data) => {
+  return data;
+}
+
 app.get('/', function(req, res){
   res.render('pages/index.ejs');
 });
+
+app.get('/chat&username=:username', (req,res) => {
+  let username = req.query.username;
+  res.render('pages/chat',{username});
+});
+
+app.post('/add-user', (req,res) => {
+  let username = req.body.username;
+  res.redirect(`/chat&username=${username}`);
+});
+
 
   socket.on('connection', function(socket){
     console.log("a new user has connected");
