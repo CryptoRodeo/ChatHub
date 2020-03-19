e9c7312e77bf675d50bb0cc696386595bcb9307e
Bryan Ramos <b.r.tech95@gmail.com>
2020-03-19 09:01:56 -0400

    basic chat features done, fixed all socket related errors, users can now set their usernames

--- public/scripts/socket_frontend.js
+++ public/scripts/socket_frontend.js
@@ -1,9 +1,23 @@
+let socket = io();
+
+(() => {
+    if(document.querySelector("#usernameForm"))
+    {
+        document.querySelector("#usernameForm").addEventListener('submit', (e) => {
+            e.preventDefault();
+            let username = document.querySelector('#username');
+            socket.emit('username', `${username.value}`);
+            console.log(username.value);
+            document.querySelector("#welcome-container").style.display = "none";
+            document.querySelector("#welcome-container").style.height = 0;
+            document.querySelector("#welcome-container").style.width = 0;
+            return false;
+        });
+    }
+})();
+
+
 (()=> {
-    const queryString = window.location.href;
-    console.log(queryString);
-    const urlParams = new URLSearchParams(queryString);
-    const username = urlParams.get('username');
-    let socket = io();
     document.querySelector("#chatform").addEventListener('submit', (e) => {
         e.preventDefault();
         let submit_button = document.querySelector("#m");
@@ -11,11 +25,12 @@
         submit_button.value = '';
         return false;
     });
-        socket.on('chat message', (msg) => {
-            let message = `<li>${username}:     ${msg}</li>`; 
-            document.querySelector("#messages").insertAdjacentHTML('beforeend', message)
+
+        socket.on('chat message', (user) => {
+            console.log(user);
+            console.log("message transmitted");
+            let message = `<li>${user.name}: ${user.message}</li>`;
+            document.querySelector("#messages").insertAdjacentHTML('beforeend', message);
         });
-        document.addEventListener('visibilitychange', (e) => {
-            console.log("user has disconnected");
-        })
+
 })();
--- server/index.js
+++ server/index.js
@@ -2,7 +2,8 @@ let express = require('express');
 let app = express();
 let partials = require('express-partials');
 let http = require('http').createServer(app);
-let socket = require('socket.io')(http);
+let io = require('socket.io')(http);
+let session = require('express-session');
 
 
 //middleware
@@ -11,41 +12,35 @@ app.use(express.static(__dirname + '/../node_modules'))
 app.use(express.static(__dirname + '/../public'));
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(session({secret: 'i got nothing to hide', resave:false, saveUninitialized: false}));
+
 
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/../views');
 
-let pass_user_date = (data) => {
-  return data;
-}
-
 app.get('/', function(req, res){
   res.render('pages/index.ejs');
 });
 
-app.get('/chat&username=:username', (req,res) => {
-  let username = req.query.username;
-  res.render('pages/chat',{username});
+io.on('connection', (socket) => {
+  let useradded = false;
+  
+  socket.on('username', (name) => {
+    if(useradded) return;
+    socket.username = name;
+    useradded = true;
 });
 
-app.post('/add-user', (req,res) => {
-  let username = req.body.username;
-  res.redirect(`/chat&username=${username}`);
-});
-
-
-  socket.on('connection', function(socket){
-    console.log("a new user has connected");
-    
-    socket.on('chat message', function(msg){
-      socket.emit('chat message', msg);
-    });
+  socket.on('chat message', function(msg){
+    let user = {};
+    user.message = msg;
+    user.name = socket.username;
+    io.emit('chat message', user);
   });
 
-  socket.on('disconnect', () => {
-    console.log("user disconnected");
-  })
+});
+
 
 http.listen(8080, function(){
   console.log('listening on *:8080');
